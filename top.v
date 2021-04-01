9f68065e416c5a879f0e7bc37dc3932057d6edf1
j-archit <archit.jain38@gmail.com>
2021-04-01 21:32:54 +0530

    D

--- top.v
+++ top.v
@@ -1,15 +1,15 @@
 /*
     Top Level Module for the MTE Project - TDDS
-    
-    Submitted to 
+
+    Submitted to
         A. Prof. Kriti Suneja
-    Submitted by 
+    Submitted by
         Archit Jain 2K18/EC/041
         Arpan Katiyar 2K18/EC/043
 
     Local Parameters and Variables/Nets/Registers
     1. System Parameters
-        Constant System Parameters 
+        Constant System Parameters
     2. BIST Circuit Common Clock
         Common Clock used by the BIST components
 
@@ -24,7 +24,7 @@
 module top;
 
 // Parameters
-// CUT 
+// CUT
 parameter INPUT_BITS = 4; // No of Inputs of CUT
 parameter OUTPUT_BITS = 4; // NO of Outputs of CUT
 parameter TOT_FAULTS = 100; // Total Number of Stuck at Faults Possible
@@ -32,15 +32,15 @@ parameter TOT_FAULTS = 100; // Total Number of Stuck at Faults Possible
 // Test Patterns
 parameter NUM_DTP = 100; // Number of Deterministic Test Patterns
 parameter NUM_RTP = 100; // Number of Random Test Patterns
-parameter ADD_BITS = 8; 
+parameter ADD_BITS = 8;
     // Width of the Memory Address for correct responses of tests
-    // Depends on total number of tests applied. 
+    // Depends on total number of tests applied.
 
-// ORA 
+// ORA
 parameter RC_BITS = 2; // Width of the Result Compactor Result
 
 // Variables/Nets/Registers
-reg clk; // Common Clock for BIST Controller 
+reg clk; // Common Clock for BIST Controller
 
 initial
 begin
